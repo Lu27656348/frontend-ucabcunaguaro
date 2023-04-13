3659dd4984f220becceb62da73005fa2acbb4183
Lu27656348 <luiscarlossomoza@gmail.com>
2023-04-13 00:44:13 -0400

    Variables de entorno fix

--- src/config.js
+++ src/config.js
@@ -2,4 +2,4 @@ import { config } from "dotenv";
 
 config()
 
-export const domain_url_env = process.env.API_DOMAIN;
+export const domain_url_env = process.env.API_DOMAIN || "http://restapi-postgres-sequelize-v3-production.up.railway.app";
