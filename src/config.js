bd17625cfb85ad87e511a860b6cd9e97a4a1eddd
Lu27656348 <luiscarlossomoza@gmail.com>
2023-04-13 00:52:53 -0400

    S

--- src/config.js
+++ src/config.js
@@ -1,3 +1,3 @@
 
 
-export const domain_url_env = "http://restapi-postgres-sequelize-v3-production.up.railway.app";
+export const domain_url_env = "https://restapi-postgres-sequelize-v3-production.up.railway.app";
