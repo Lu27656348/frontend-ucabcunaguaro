162bc08e8fec9f0d819dc77e13fcfbae32d9eca7
Lu27656348 <luiscarlossomoza@gmail.com>
2023-04-16 01:50:05 -0400

    Ucab cunaguaro - V

--- src/config.js
+++ src/config.js
@@ -1,4 +1,4 @@
 
 
-//export const domain_url_env = "https://restapi-postgres-sequelize-v3-production.up.railway.app";
-export const domain_url_env = "http://localhost:3000";
+export const domain_url_env = "https://restapi-postgres-sequelize-v3-production.up.railway.app";
+//export const domain_url_env = "http://localhost:3000";
